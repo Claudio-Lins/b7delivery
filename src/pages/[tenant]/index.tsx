23f176ccf27d75a4ceea083ab3330514e4319816
Claudio Lins <claudio.lins@me.com>
2023-04-28 21:04:33 +0100

    /* eslint-disable react-hooks/exhaustive-deps */

--- src/pages/[tenant]/index.tsx
+++ src/pages/[tenant]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import type { GetServerSideProps } from 'next'
 import { useEffect, useState } from 'react'
 import { IoMdMenu } from 'react-icons/io'
